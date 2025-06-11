4dc76b807459bafc8acf92691ebf0d02f070edc0
Iashkina Adelina <yadelishka@gmail.com>
2025-06-11 15:39:33 +0300

    finish static part

--- src/App.js
+++ src/App.js
@@ -20,5 +20,91 @@ const initialFriends = [
 ];
 
 export default function App() {
-  return <div>Hello!</div>;
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+        <FormAddFriend />
+        <Button>Add friend</Button>
+      </div>
+      <FormSplitBill />
+    </div>
+  );
+}
+
+function FriendsList() {
+  const friends = initialFriends;
+
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} {Math.abs(friend.balance)}€
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {Math.abs(friend.balance)}€
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+      <Button>Select</Button>
+    </li>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>🧑‍🤝‍🧑Friend name</label>
+      <input type="text"></input>
+
+      <label>🖼️Image URL</label>
+      <input type="text"></input>
+
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+
+      <label>💰Bill value</label>
+      <input type="text"></input>
+
+      <label>🧍‍♂️Your expense</label>
+      <input type="text"></input>
+
+      <label>🧑‍🤝‍🧑X's expense</label>
+      <input type="text" disabled></input>
+
+      <label>🤑Who is paying the bill?</label>
+      <select>
+        <option value="user">You</option>
+        <option value="freiend">X</option>
+      </select>
+
+      <Button>Add</Button>
+    </form>
+  );
 }
